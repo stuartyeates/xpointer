ccfdb7f86279d1f7602f4a9892fe8153d414be6f
stuart yeates <syeates@gmail.com>
2012-07-08 13:50:55 +1200

    break xinclude out into seperate file, fix mangling

--- Main.rb
+++ Main.rb
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 require "rexml/document"
+require "lib/xinclude"
 include REXML 
 
 TINY_DOC = "<doc><one></one><two></two></doc>"  
@@ -27,63 +28,6 @@ puts "=========== find an item ==========="
 four = XPath.first( doc, "//four[@xml:id='four']" ) 
 puts four
 puts "=========== end ==========="
-
-# A class implementing XML Inclusions (XInclude)
-#
-# See:
-# [http://www.w3.org/TR/xinclude/] for the standard
-class XInclude
-  NAMESPACE = "http://www.w3.org/2001/XInclude"
-  @replacements = {}
-
-  #a function to process all the includes in an REXML-parsed XML doc
-  #returns the number of includes processed
-  def process(doc)
-    @replacements = {}
-    puts doc
-
-    puts  XPath.first( doc, "//xi:xinclude", {'xi' => NAMESPACE} )
-    #XPath.match( doc, "//xi:xinclude", {'xi' => NAMESPACE} ).each() { |element| processInclude(element) }
-    copyDocumentWithReplacements(doc)
-  end
-
-  def copyElementWithReplacements(element, newElement)
-    element.elements.each() do |thisElement|
-      if (thisElement.expanded_name == 'xi:xinclude') then
-        thisElement = processInclude(thisElement)
-      end
-      newThisElement = Element.new(thisElement)
-      newElement.add_element(newThisElement)
-      copyElementWithReplacements(thisElement,newThisElement)
-    end
-  end
-
-  #a function to process a single element in an  REXML-parsed XML doc
-  #returns the new element
-  def processInclude(element)
-    puts element
-    newElement =  processURI(element.attributes["href"],
-                             element.attributes["parse"],
-                             element.attributes["xpointer"],
-                             element.attributes["encoding"],
-                             element.attributes["accept"],
-                             element.attributes["accept-language"])
-    @replacements[element] = newElement.root
-  end
-  
-  #build the inclusion. throws any errors that might occur
-  def processURI(href, parse, xpointer, encoding, accept, acceptlanguage)
-    #catch some fatal errors specified by the spec
-    if (parse != nil && parse != "text" && parse != "xml")
-        raise new Exception
-    end
-    if (parse == "text" && xpointer != nil)
-        raise new Exception
-    end
-    puts href
-    doc = REXML::Document.new(File.new(href));
-  end  
-end
 
 
 doc = REXML::Document.new(XINCLUDEDOC)
