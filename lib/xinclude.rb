dabf49f857834a289d0dee141c00d7578a68f295
stuart yeates <syeates@gmail.com>
2012-07-08 17:54:47 +1200

    make text nodes work

--- lib/xinclude.rb
+++ lib/xinclude.rb
@@ -10,6 +10,7 @@ class XInclude
   #a function to process all the includes in an REXML-parsed XML doc
   #returns the number of includes processed
   def process(doc)
+    raise unless doc.is_a?(Document)
 
     #puts  XPath.first( doc, "//xi:xinclude", {'xi' => NAMESPACE} )
     #XPath.match( doc, "//xi:xinclude", {'xi' => NAMESPACE} ).each() { |element| processInclude(element) }
@@ -19,20 +20,32 @@ class XInclude
  end
 
   def copyElementWithReplacements(element, newElement)
-    element.elements.each() do |thisElement|
-      if (thisElement.expanded_name == 'xi:xinclude') then
-        thisElement = processInclude(thisElement).root
+    raise unless element.is_a?(Element)
+    raise unless newElement.is_a?(Element)
+
+    element.children.each() do |child|
+      if (child.is_a?(Element)) then
+        if (child.expanded_name == 'xi:xinclude') then
+          child = processInclude(child).root
+        end
+        newChild = Element.new(child)
+        newElement.add(newChild)
+        
+        copyElementWithReplacements(child,newChild)
+      elsif (child.is_a?(Text))
+        newChild = Text.new(child)
+        newElement.add(newChild)
       end
-      newThisElement = Element.new(thisElement)
-      newElement.add_element(newThisElement)
-      copyElementWithReplacements(thisElement,newThisElement)
+      #puts "==" + child.to_s() + "=="
     end
   end
 
   #a function to process a single element in an  REXML-parsed XML doc
   #returns the new element
   def processInclude(element)
-    puts element
+    raise unless element.is_a?(Element)
+    raise unless element.expanded_name == 'xi:xinclude'
+
     newElement =  processURI(element.attributes["href"],
                              element.attributes["parse"],
                              element.attributes["xpointer"],
@@ -43,14 +56,11 @@ class XInclude
   
   #build the inclusion. throws any errors that might occur
   def processURI(href, parse, xpointer, encoding, accept, acceptlanguage)
-    #catch some fatal errors specified by the spec
-    if (parse != nil && parse != "text" && parse != "xml")
-        raise new Exception
-    end
-    if (parse == "text" && xpointer != nil)
-        raise new Exception
-    end
-    puts href
+    # catch some fatal errors specified by the spec
+    raise if (parse != nil && parse != "text" && parse != "xml")
+    raise if (parse == "text" && xpointer != nil)
+    # we can't do anything without an href
+    raise if (href == nil)
     doc = REXML::Document.new(File.new(href));
   end  
 end
