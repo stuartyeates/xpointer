cc8ee96f1926a197fe81267345bcde8856bb7c0f
stuart yeates <syeates@gmail.com>
2012-07-12 22:11:52 +1200

    element() half done + appropiate tests

--- lib/xpointer.rb
+++ lib/xpointer.rb
@@ -9,8 +9,37 @@ class XPointer
   NAMESPACE = "http://www.w3.org/2001/05/XPointer"
   
  def  process(document, xpointer)
-   return document
+   raise "Bad document passed to Xpointer::process" unless document
+
+   case xpointer
+   when nil, '', '#'
+     document
+   when /^element\(/i
+     document = processElement(document, xpointer)
+   when /^string-range\(/i
+     document = processStringRange(document, xpointer)
+   else
+     raise "Bad xpointer at XPointer::process(#{xpointer})"
+   end
  end
 
+   def processElement(document, xpointer)
+     case xpointer
+     when nil, '', '#'
+       document
+     when /^element\(([A-Za-z0-9]*)((\/[0-9]*)*)\)/i
+       puts "found a live one #{$1}"
+       document =  REXML::XPath.first( document, "//#{$1}" ) 
+       document
+     else
+       raise "Bad xpointer at XPointer::processElement(#{xpointer})"
+   end
+
+     return document
+   end
+
+   def processStringRange(document, xpointer)
+     return document
+   end
 
 end 
--- test/xpointer.rb
+++ test/xpointer.rb
@@ -1,5 +1,7 @@
 require "lib/xpointer"
 require "test/unit"
+require "rexml/document"
+require "lib/xinclude"
 
  
 class TestXPointer < Test::Unit::TestCase
@@ -7,5 +9,54 @@ class TestXPointer < Test::Unit::TestCase
   def test_instantiation
     assert_not_nil(XPointer.new())
   end
+
+  XINCLUDEDOC1="<doc xmlns:xi='http://www.w3.org/2001/XInclude'><xi:xinclude href='./test/samples/tahi.xml' xpointer=''/></doc>"
+  
+  def test_1
+    doc = REXML::Document.new(XINCLUDEDOC1)
+    assert_not_nil(doc)
+    xinclude = XInclude.new()
+    assert_not_nil(xinclude)
+    doc = xinclude.process(doc)
+    asstring = doc.to_s()
+    assert_not_nil(/ kore /.match(asstring))
+  end
+
+  XINCLUDEDOC2="<doc xmlns:xi='http://www.w3.org/2001/XInclude'><xi:xinclude href='./test/samples/tahi.xml' xpointer='element()'/></doc>"
+  
+  def test_2
+    doc = REXML::Document.new(XINCLUDEDOC2)
+    assert_not_nil(doc)
+    xinclude = XInclude.new()
+    assert_not_nil(xinclude)
+    doc = xinclude.process(doc)
+    asstring = doc.to_s()
+    assert_not_nil(/ kore /.match(asstring))
+  end
+
+  XINCLUDEDOC3="<doc xmlns:xi='http://www.w3.org/2001/XInclude'><xi:xinclude href='./test/samples/tahi.xml' xpointer='#'/></doc>"
+  
+  def test_3
+    doc = REXML::Document.new(XINCLUDEDOC3)
+    assert_not_nil(doc)
+    xinclude = XInclude.new()
+    assert_not_nil(xinclude)
+    doc = xinclude.process(doc)
+    asstring = doc.to_s()
+    assert_not_nil(/ kore /.match(asstring))
+  end
+
+  XINCLUDEDOC4="<doc xmlns:xi='http://www.w3.org/2001/XInclude'><xi:xinclude href='./test/samples/tahi.xml' xpointer='element(one)'/></doc>"
+  
+  def test_4
+    doc = REXML::Document.new(XINCLUDEDOC4)
+    assert_not_nil(doc)
+    xinclude = XInclude.new()
+    assert_not_nil(xinclude)
+    doc = xinclude.process(doc)
+    asstring = doc.to_s()
+    assert_not_nil(/ kore /.match(asstring))
+  end
+
 
 end
